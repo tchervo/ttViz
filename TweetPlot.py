5de5526da0e0e2f7c3aea3bc980bb010969a94d5
tchervo <58800120+tchervo@users.noreply.github.com>
2019-12-21 18:46:00 -0800

    Redesigned main method. Addes user search support.
    Also changed tweet saving to include more information such as date and time

--- TweetPlot.py
+++ TweetPlot.py
@@ -52,93 +52,99 @@ def login(account_data: pd.DataFrame):
 
         auth.set_access_token(a_t, a_s)
 
-# Loads tweets either from file (default) or from Twitter
-def search_tweets(topic: str, load_tweets=True) -> list:
-    tweet_text = []
 
-    if load_tweets:
-        tweet_text = dm.load_tweets(topic=topic)
-    else:
-        print("Loading Twitter!")
-        tweets = tw.Cursor(api.search, q=topic).items(50)
-
-        for tweet in tweets:
-            tweet_text.append(tweet.text)
-
-    return tweet_text
-
-
-def do_stats(frame: pd.DataFrame):
-    total_word_sum = sci.sum(frame["Freq"])
-    target_word_sum = frame.iloc[0, 1]
-
-    print(target_word_sum)
-
-    print(str(total_word_sum), '\n', str(target_word_sum), '\n', str(target_word_sum / total_word_sum))
-
+# # Loads tweets either from file (default) or from Twitter - Currently unused
+# def search_tweets(topic: str, load_tweets=True) -> list:
+#     tweet_text = []
+#
+#     if load_tweets:
+#         tweet_text = dm.load_tweets(topic)
+#     else:
+#         print("Loading Twitter!")
+#         print(topic)
+#         tweets = tw.Cursor(api.search, q=topic).items(100)
+#
+#         for tweet in tweets:
+#             tweet_text.append(tweet.text)
+#
+#     print(len(tweet_text))
+#     return tweet_text
 
 def main():
-    login(load_account_data())
+    account_data = load_account_data()
+    login(account_data)
 
-    mode = int(input("Select Mode (1 - Frequency | 2 - Association): "))
-    should_save = input("Save tweets?: ")
-    search = input("Input search term: ")
+    mode = input("Select search mode: Topic (1) or User (2)")
 
-    if should_save.lower() == "yes":
-        if len(search.split(",")) >= 2:
-            for term in search.split(","):
-                dm.save_tweets(topic=term)
+    if mode == '1':
+        topic = input("Select a topic to search: ")
+        dm.save_tweets(topic)
+        topic_file = dm.make_file_name_for_search(topic)
+        topic_frame = dm.get_dataframe_from_file(topic_file)
 
-                tweets = search_tweets(topic=term, load_tweets=True)
-                data = dm.strip_tweet(tweets)
-                frame = None
-
-                if mode == 1:
-                    frame = dm.build_dataframe(data)
-                elif mode == 2:
-                    frame = dm.build_dataframe(data)
-        else:
-            dm.save_tweets(topic=search)
-
-            tweets = search_tweets(topic=search, load_tweets=True)
-            data = dm.strip_tweet(tweets)
-            frame = None
-
-            if mode == 1:
-                frame = dm.build_dataframe(data)
-            elif mode == 2:
-                frame = dm.build_dataframe(data)
+        print(topic_frame.head())
+        print(topic_frame.tail())
+    elif mode == '2':
+        username = input("Input username: ")
+        dm.save_tweets(topic=username, do_search=False, to_save=dm.get_tweets_for_user(username))
     else:
-        if len(search.split(",")) >= 2:
-            for term in search.split(','):
-                tweets = search_tweets(topic=term, load_tweets=True)
-                data = dm.strip_tweet(tweets)
-                frame = None
-                if mode == 1:
-                    frame = dm.build_dataframe(data)
-                elif mode == 2:
-                    frame = dm.build_dataframe(data)
-                else:
-                    pass
-        else:
-            tweets = search_tweets(topic=search, load_tweets=True)
-            data = dm.strip_tweet(tweets)
-            frame = None
-            if mode == 1:
-                frame = dm.build_dataframe(data)
-            elif mode == 2:
-                frame = dm.build_dataframe(data)
-            else:
-                pass
+        print('Invalid input!')
+        main()
 
-    print(frame.head(30))
-    # do_stats(frame)
-
-    if input("Run again?: ").capitalize() == "Y":
+    if input("Run again?: ").capitalize().startswith('Y'):
         main()
     else:
         print("Exiting...")
 
-
 if __name__ == '__main__':
     main()
+
+# Old main method, currently being replaced
+# login(load_account_data())
+#
+#     mode = int(input("Select Mode (1 - Frequency | 2 - Association): "))
+#     should_save_tweets = input("Save tweets?: ")
+#     should_save_words = input("Save words?: ")
+#     search = input("Input search term: ")
+#     frames = []
+#
+#     if True:
+#         if len(search.split(",")) >= 2:
+#             for term in search.split(","):
+#                 dm.save_tweets(topic=term)
+#
+#                 tweets = search_tweets(topic=term, load_tweets=True)
+#                 data = dm.strip_tweet(tweets)
+#                 frame = None
+#
+#                 if mode == 1:
+#                     frame = dm.build_dataframe(data)
+#                 elif mode == 2:
+#                     frame = dm.build_dataframe(data)
+#
+#                 frames.append(frame)
+#         else:
+#             print("YES")
+#             dm.save_tweets(topic=search)
+#             tweets = dm.load_tweets(topic=search)
+#             data = dm.strip_tweet(tweets)
+#             frame = None
+#
+#             if mode == 1:
+#                 frame = dm.build_dataframe(data)
+#             elif mode == 2:
+#                 frame = dm.build_dataframe(data)
+#
+#             frames.append(frame)
+#
+#     for tweet in get_tweets_for_user(input("Username: ")):
+#         print(tweet)
+#
+#     for frame in frames:
+#         print(frame.head(30))
+#         print(frame.tail(30))
+#
+#         if True:
+#             print("Saving Frame!!!")
+#             for term in search.split(','):
+#                 frame.to_csv(term.lower().replace(' ', '_') + "_words.csv")
--- datamanager.py
+++ datamanager.py
@@ -1,76 +1,159 @@
 import pandas as pd
 from nltk.corpus import stopwords
 import TweetPlot
+import scipy as sci
 
 tw = TweetPlot.tw
 api = TweetPlot.api
 
 
+def make_file_name_for_search(search: str, type='tweets') -> str:
+    save_file = ''
+
+    if len(search.split(',')) >= 2:
+        for topic in search.split(','):
+            if len(topic.split(' ')) >= 2:
+                for word in topic.split(' '):
+                    if save_file is '':
+                        save_file = word.lower()
+                    else:
+                        save_file = save_file + f'_{word.lower()}'
+                        print(save_file)
+                save_file = save_file + '_{0}.csv'.format(type)
+            else:
+                save_file = '{0}_{1}.csv'.format(search.lower(), type)
+
+    else:
+        if len(search.split(' ')) >= 2:
+            for word in search.split(' '):
+                if save_file is '':
+                    save_file = word.lower()
+                else:
+                    save_file = save_file + f'_{word.lower()}'
+                    print(save_file)
+            save_file = save_file + '_{0}.csv'.format(type)
+        else:
+            save_file = '{0}_{1}.csv'.format(search.lower(), type)
+
+    return save_file
+
+
 # Saves tweets to a CSV file named after their topic
-def save_tweets(topic: str):
+def save_tweets(topic: str, do_search=True, to_save=[]):
     tweet_text = []
     tweet_ids = []
     tweet_favorites = []
     tweet_retweets = []
-    tweets = tw.Cursor(api.search, q=topic).items(50)
+    tweet_screen_names = []
+    tweet_times = []
+    tweet_dates = []
+    save_file = make_file_name_for_search(topic)
 
-    for tweet in tweets:
-        tweet_text.append(tweet.text)
-        tweet_ids.append(tweet.id)
-        tweet_favorites.append(int(tweet.favorite_count))
-        tweet_retweets.append(int(tweet.retweet_count))
+    if do_search:
+        tweets = tw.Cursor(api.search, q=topic + ' -filter:retweets', lang="en", result_type='popular').items(100)
 
-    tweet_frame = pd.DataFrame(data={"Tweet ID": tweet_ids, "Text": tweet_text, "Favorites": tweet_favorites,
-                                     "Retweets": tweet_retweets})
-    tweet_frame.to_csv(topic + ".csv")
+        for tweet in tweets:
+            tweet_text.append(tweet.text)
+            tweet_ids.append(tweet.id)
+            tweet_favorites.append(int(tweet.favorite_count))
+            tweet_retweets.append(int(tweet.retweet_count))
+            tweet_screen_names.append(tweet.user.screen_name)
+            tweet_times.append(str(tweet.created_at))
 
-    test = pd.read_csv(topic + ".csv")
+        tweet_frame = pd.DataFrame(data={"tweet_ID": tweet_ids, "text": tweet_text, "favorites": tweet_favorites,
+                                         "retweets": tweet_retweets, "screen_name": tweet_screen_names,
+                                         "tweet_times": tweet_times})
+        tweet_frame.to_csv(save_file)
+    else:
+        for tweet in to_save:
+            tweet_text.append(tweet.text)
+            tweet_ids.append(tweet.id)
+            tweet_favorites.append(int(tweet.favorite_count))
+            tweet_retweets.append(int(tweet.retweet_count))
+            tweet_screen_names.append(tweet.user.screen_name)
+            tweet_times.append(str(tweet.created_at))
 
-    print(test.head())
+        tweet_frame = pd.DataFrame(data={"tweet_ID": tweet_ids, "text": tweet_text, "favorites": tweet_favorites,
+                                         "retweets": tweet_retweets, "screen_name": tweet_screen_names,
+                                         "tweet_times": tweet_times})
+        tweet_frame.to_csv(save_file)
 
 
 # Loads tweets from CSV and returns an array of the tweets' text
 def load_tweets(topic: str) -> [str]:
     tweet_text = []
-    file_name = topic + ".csv"
-    tweet_frame = pd.read_csv(file_name)
+    file_name = make_file_name_for_search(topic)
 
-    print(tweet_frame.head())
+    tweet_frame = get_dataframe_from_file(file_name)
 
-    for text in tweet_frame.Text:
+    for text in tweet_frame.text:
         tweet_text.append(text)
 
     return tweet_text
 
 
-# Assembles a pandas dataframe out of the frequency of specific words
-def build_dataframe(data: []) -> pd.DataFrame:
-    word_counter = {}
-    word_list = []
-    freq_list = []
+def get_dataframe_from_file(file_name: str) -> pd.DataFrame:
+    ret_frame = None
 
-    for word in data:
-
-        if word in word_counter.keys():
-            new_count = int(word_counter[word]) + 1
-            word_counter[word] = new_count
-        else:
-            word_counter[word] = 1
-
-    for word in word_counter.keys():
-        word_list.append(word)
-    for freq in word_counter.values():
-        freq_list.append(freq)
-
-    ret_frame = pd.DataFrame({"Word": word_list, "Freq": freq_list}).sort_values(by=["Freq"], ascending=False)
+    try:
+        ret_frame = pd.read_csv(file_name)
+    except IOError as error:
+        print(f'An error occured loading dataframe with name: {file_name}! (Wrong name?)')
+        print(error)
 
     return ret_frame
+
+
+def get_tweets_for_user(username: str, filter_retweets=True) -> [str]:
+    user = None
+    tweets = []
+
+    try:
+        user = api.get_user(username)
+    except tw.TweepError as error:
+        print(f'Could not find user with username: {username} because {error.reason}')
+        print(error.api_code)
+
+    if user.protected is not True:
+        if filter_retweets:
+            for tweet in api.user_timeline(user.id):
+                if str(tweet.text).startswith('RT') is False:
+                    tweets.append(tweet)
+
+            return tweets
+        else:
+            return api.user_timeline(user.id)
+    else:
+        return "PRIVATE"
+
+
+# Assembles a pandas dataframe out of the frequency of specific words - Currently unused
+# def build_dataframe(data: []) -> pd.DataFrame:
+#     word_counter = {}
+#     word_list = []
+#     freq_list = []
+#
+#     for word in data:
+#
+#         if word in word_counter.keys():
+#             new_count = int(word_counter[word]) + 1
+#             word_counter[word] = new_count
+#         else:
+#             word_counter[word] = 1
+#
+#     for word in word_counter.keys():
+#         word_list.append(word)
+#     for freq in word_counter.values():
+#         freq_list.append(freq)
+#
+#     ret_frame = pd.DataFrame({"word": word_list, "freq": freq_list}).sort_values(by=["freq"], ascending=False)
+#
+#     return ret_frame
 
 
 # Removes 'RT' and '#' from tweets and selects for meaningful words
 def strip_tweet(whole_tweet_list: str) -> [str]:
     stripped_tweet = []
-    punc = ['.', ',', ';', '?', '!']
 
     for whole_tweet in whole_tweet_list:
         for phrase in whole_tweet.split(" "):
